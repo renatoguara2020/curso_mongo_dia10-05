719d1a8e13ce65fc4f4ff946d93eed08f28db182
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-10 11:49:46 -0300

    commit changes

--- ServerExpress.js
+++ ServerExpress.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use(express.json);
+
 app.get('/', function (req, res) {
     res.send('<html><body><h1>Hello World com Express dia 10/05</h1></body></html>');
+});
+
+app.get('/id:', function (req, res) {
+
+    res.send('<html><body><h1>Hello World com Express e ID do GET')
 });
 
 app.post('/submit-data', function (req, res) {
