865603a9aa1f8127bf5522bdbf7c785d46213ca5
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-10 19:12:17 -0300

    commit changes

--- nodeMailer.js
+++ nodeMailer.js
@@ -6,7 +6,7 @@ var transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   auth: {
     user: 'renatoguara2020@gmail.com',
-    pass: 'dtkisoqxwwhnxpgtvvv'
+    pass: 'dtkisoqxwwhnxpgtmnbvcxz'
   }
 });
 
