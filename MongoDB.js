bfc8ea8927ee3254e5bfdda33b96723c67337a87
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-10 09:00:21 -0300

    ok

---
+++ MongoDB.js
@@ -0,0 +1,11 @@
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/primeirobanco", function (err, db) {
+   
+     if(err) throw err;
+
+     //Write databse Insert/Update/Query code here..
+     console.log('Conectou ao MongoDB')
+                
+});
